82506000964cd8a32049da676483a4fc18809027
KBthumar180 <kavya.thummar@gmail.com>
2025-09-19 11:01:02 +0530

    19/9

--- pr-1/src/App.jsx
+++ pr-1/src/App.jsx
@@ -2,33 +2,31 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import UserProfile from './Components/UserProfile'
+import img from "./assets/man-1.webp"
+import img1 from "./assets/man-2.jpeg"
+import img2 from "./assets/man-3.jpeg"
+import img3 from "./assets/man-4.jpeg"
+import img4 from "./assets/man-5.jpeg"
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>User Imfornation</h1>
+    <div className="user-grid">
+       <UserProfile name = "meet" profilePic={img} email="trapasiya@.com" location="Surat" age="19" Occupation="Full-stack Developer"/>
+
+     <UserProfile name = "darshik" profilePic={img1} email="darshik@.com" location="Surat" age="23" Occupation="UI-UX Designer"/>
+
+      <UserProfile name = "lav" profilePic={img2} email="lav786@.com" location="Surat" age="22" Occupation="Front-End Developer"/>
+      
+       <UserProfile name = "krish" profilePic={img3} email="pajiyawala@.com" location="Surat" age="20" Occupation="Front-End Developer"/>
+
+        <UserProfile name = "jaydeep" profilePic={img4} email="jdVora@.com" location="Surat" age="18" Occupation="Game-Developer"/>
+    </div>
+</div>
+
   )
 }
 
---
+++ pr-1/src/Components/UserProfile.jsx
@@ -0,0 +1,35 @@
+ 
+import React from "react";
+import "../css/UserProfile.css"; 
+ 
+class UserProfile extends React.Component {
+
+    constructor(props){
+        super()
+    }
+  render() {
+    const { user } = this.props;   
+
+    return (
+      <div className="user-profile">
+        <div className="profile-header">
+          <img
+            className="profile-pic"
+            src={this.props.profilePic} 
+            alt={`${this.props.name}'s profile`} 
+          />
+          <h2 className="user-name">{this.props.name}</h2>
+          <hr/>
+        </div>
+        <div className="profile-details">
+              <p><strong>Occupation  :</strong> {this.props.Occupation}</p>
+          <p><strong>Email  :</strong> {this.props.email}</p>
+          <p><strong>Location  :</strong> {this.props.location}</p>
+          <p><strong>Age  :</strong> {this.props.age}</p>
+        </div> 
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
